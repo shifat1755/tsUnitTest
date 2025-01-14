c2b794818a2ebe16c304bee18bae17cde2de0584
Syed Shifat <SyedShifat.Hasan@optimizely.com>
2025-01-14 21:35:10 +0600

    fix: gitworkflow2

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const dbURI = 'mongodb://localhost:27017/nodeTs';
 
 export const dbConnection = async () => {
+  if (process.env.CI === 'true') {
+    console.log('Skipping MongoDB connection in CI environment');
+    return;
+  }
     try {
       await mongoose.connect(dbURI);
       console.log("dbConnected");
